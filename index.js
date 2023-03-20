c3431ef6079cc8c961d6ed468ba203a7be995a9b
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-03-20 20:11:36 +0100

    up to caesarCipher

---
+++ index.js
@@ -0,0 +1,56 @@
+function capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
+}
+
+function reverseString(string) {
+    return string.split("").reverse().join("");
+}
+
+const calculator = {
+    add: function(n, m) {
+        return n + m;
+    }, 
+    subtract: function(n, m) {
+        return n - m;
+    }, 
+    divide: function(n, m) {
+        return n / m;
+    }, 
+    multiply: function(n, m) {
+        return n *m;
+    }, 
+}
+
+function caesarCipher(string, key) {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    const alphabetArray = alphabet.split("");
+    const stringToArray = string.split("");
+    let finalCypher = "";
+
+    for (let i = 0; i < stringToArray.length; i++) {
+        let letterPlace = findInAlphabet(stringToArray[i]);
+        finalCypher += shiftLetter(letterPlace);
+    }
+    
+    function findInAlphabet(letter) {
+        for (let i = 0; i < alphabetArray.length; i++) {
+            if (letter == alphabetArray[i]) {
+                return alphabetArray.indexOf(alphabet[i]);
+            }
+        }
+    }
+
+    function shiftLetter(letterPlace) {
+        if (letterPlace + key >= alphabetArray.length) {
+            return alphabetArray[(letterPlace + key) % alphabetArray.length] 
+        }
+        return alphabetArray[letterPlace + key];
+    }
+
+    return finalCypher
+}
+
+function analyzeArray() {
+
+}
+ console.log(caesarCipher("abczy", 25))
